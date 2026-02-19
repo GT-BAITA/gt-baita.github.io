125524e3bb9f655d12a3e17d63deba285b9205a0
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-02-18 22:10:23 -0300

    fix: using HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/redirect" element={<RandomRedirect/>}/>
+      <Route path="/redirect" element={<RandomRedirect />}/>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './styles/global.css';
 import './locale';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GoogleReCaptchaProvider
         reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
       >
         <App />
       </GoogleReCaptchaProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
