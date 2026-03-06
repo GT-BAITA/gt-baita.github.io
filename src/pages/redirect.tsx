eea731172e3ba7759d119f10dc5cb91129b9bc1b
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-03-05 23:27:34 -0300

    fix: giving more weight to non-traditional WAYF tests

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -2,20 +2,32 @@ import { useEffect } from 'react';
 
 function RandomRedirect() {
   useEffect(() => {
-    const urlsString = import.meta.env.VITE_REDIRECT_URLS_TO_TEST_WAYF || "";
+    // https://t.maze.co/501900855 -> WAYF ATUAL
+    // https://t.maze.co/501964025 -> BUSCA INLINE
+    // https://t.maze.co/502054013 -> DRILL DOWN
 
-    const urls = urlsString.split(',')
-      .map((url: string) => url.trim())
-      .filter((url: string | any[]) => url.length > 0);
+    const urlWeights = [
+      { url: "https://t.maze.co/501900855", weight: 1 },
+      { url: "https://t.maze.co/501964025", weight: 2 },
+      { url: "https://t.maze.co/502054013", weight: 2 } 
+    ];
 
-    if (urls.length > 0) {
-      const randomIndex = Math.floor(Math.random() * urls.length);
-      const selectedUrl = urls[randomIndex];
+    const weightedPool: string[] = [];
+    urlWeights.forEach(item => {
+      for (let i = 0; i < item.weight; i++) {
+        weightedPool.push(item.url);
+      }
+    });
 
-      if (selectedUrl.startsWith('http://') || selectedUrl.startsWith('https://')) {
+    if (weightedPool.length > 0) {
+      const randomIndex = Math.floor(Math.random() * weightedPool.length);
+      const selectedUrl = weightedPool[randomIndex];
+
+      if (selectedUrl.startsWith('http')) {
         window.location.replace(selectedUrl);
       }
     }
+
   }, []);
 
   return <h3>Redirecionando...</h3>;
